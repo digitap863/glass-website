3f5be1f5508a58b07e1ff9aa0872b1d77150e93c
rajeev <rajeevtk27@gmail.com>
2021-04-10 15:25:03 +0530

    changes updated

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -2,7 +2,9 @@ jQuery(document).ready(function($) {
   "use strict";
 
   //Contact
+  
   $('form.contactForm').submit(function() {
+   
     var f = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
